4c914e4443977c6df892446f7ecdbbca568146ff
casjay <git-admin@casjaysdev.com>
2022-10-07 15:18:29 -0400

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ Dockerfile
@@ -0,0 +1,62 @@
+FROM rockylinux:8 AS build
+
+ARG LICENSE=WTFPL \
+  IMAGE_NAME=rhel8 \
+  TIMEZONE=America/New_York \
+  PORT=
+
+ENV SHELL=/bin/bash \
+  TERM=xterm-256color \
+  HOSTNAME=${HOSTNAME:-casjaysdev-$IMAGE_NAME} \
+  TZ=$TIMEZONE \
+  RPM_SOURCE_DIR="/data/rpmbuild" \
+  RPM_RELEASE_DIR="/data/release"
+
+RUN mkdir -p /bin/ /config/ /data/ && \
+  rm -Rf /bin/.gitkeep /config/.gitkeep /data/.gitkeep && \
+  yum update -y && \
+  yum install wget curl git -y && \
+  yum clean all && \
+  wget -q "https://github.com/rpm-devel/casjay-release/raw/main/casjay.rh8.repo" -O "/etc/yum.repos.d/casjay.repo" && \
+  yum update -y && \
+  yum groupinstall "Development Tools" "RPM Development Tools" -y && \
+  yum clean all
+
+COPY ./config/rpmmacros /root/.rpmmacros
+COPY ./bin/. /usr/local/bin/
+COPY ./config/. /config/
+COPY ./data/. /data/
+
+FROM scratch
+ARG BUILD_DATE="$(date +'%Y-%m-%d %H:%M')"
+
+LABEL org.label-schema.name="rhel8" \
+  org.label-schema.description="Containerized version of rhel8" \
+  org.label-schema.url="https://hub.docker.com/r/casjaysdevdocker/rhel8" \
+  org.label-schema.vcs-url="https://github.com/casjaysdevdocker/rhel8" \
+  org.label-schema.build-date=$BUILD_DATE \
+  org.label-schema.version=$BUILD_DATE \
+  org.label-schema.vcs-ref=$BUILD_DATE \
+  org.label-schema.license="$LICENSE" \
+  org.label-schema.vcs-type="Git" \
+  org.label-schema.schema-version="latest" \
+  org.label-schema.vendor="CasjaysDev" \
+  maintainer="CasjaysDev <docker-admin@casjaysdev.com>"
+
+ENV SHELL="/bin/bash" \
+  TERM="xterm-256color" \
+  HOSTNAME="casjaysdev-rhel8" \
+  TZ="${TZ:-America/New_York}"
+
+WORKDIR /root
+
+VOLUME ["/root","/config","/data/rpmbuild","/data/release"]
+
+EXPOSE $PORT
+
+COPY --from=build /. /
+
+ENTRYPOINT [ "tini", "--" ]
+HEALTHCHECK CMD [ "/usr/local/bin/entrypoint-rhel8.sh", "healthcheck" ]
+CMD [ "/usr/local/bin/entrypoint-rhel8.sh" ]
+
---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env sh
+# shellcheck shell=sh
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :
+# @Author            :  Jason
+# @Contact           :  jason@casjaysdev.com
+# @License           :  WTFPL
+# @ReadME            :  build --help
+# @Copyright         :  Copyright (c) 2022, Casjays Developments
+# @Created           :  Friday Oct 08, 2022 10:44:35 EDT
+# @File              :  build
+# @@Description      :  script to build docker images
+# @@Changelog        :
+# @@TODO             :
+# @@Other            :
+# @@Resource         :
+# @@Terminal App     :  no
+# @@sudo/root        :  no
+# @@Template         :  shell/sh
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set variables
+build_platforms="linux/amd64,linux/arm64"
+hub_username="casjaysdevdocker"
+release="8"
+arguments="$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main application
+if docker buildx 2>&1 | grep -q 'docker buildx COMMAND'; then
+  echo "Building docker image with buildx"
+  sudo docker buildx build \
+      --platform "$build_platforms" \
+      --rm \
+      --pull \
+      --no-cache \
+      --progress auto \
+      --output=type=registry \
+      -t $hub_username/rhel$release:latest \
+      -t $hub_username/rpm-devel:$release $arguments \
+      -f "$PWD/Dockerfile" "$PWD"
+      exit $?
+else
+  echo "Building docker image with docker"
+    sudo docker build \
+      -f "$PWD/Dockerfile" \
+      --pull \
+      --force-rm \
+      --no-cache \
+      -t $hub_username/rhel$release:latest \
+      -t $hub_username/rpm-devel:$release $arguments "$PWD"
+      if [ "$?" = 0 ]; then
+        sudo docker push $hub_username/rhel$release:latest
+        sudo docker push $hub_username/rpm-devel:$release
+        sudo docker rmi -f $hub_username/rhel$release:latest
+        sudo docker rmi -f $hub_username/rpm-devel:$release
+      fi
+      exit $?
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End application
+
